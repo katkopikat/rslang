134dd923902311c4c7057b98783a331e90640705
Katy Rog <katkopikat@gmail.com>
2021-03-28 22:17:13 +0300

    feat: finshed logic game

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import WriteWord from '../Games/WriteWord';
+import VocabularyPage from '../VocabularyPage/VocabularyPage';
 
 // import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const App: React.FC = () => (
-  <Router>
-    {/* <Header /> */}
-    <WriteWord group={1} page={1} />
-    <Footer />
-  </Router>
-);
+const App: React.FC = () => {
+  // temp data:
+  const group: number = 0;
+  const page: number = 0;
+
+  return (
+
+    <Router>
+      {/* <Header /> */}
+      <VocabularyPage group={group} page={page} />
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
