d6b11b817c63c96a1fa3b5518b2bad400e6b33c2
Aliaksei Akulau <akulaualiaksei@gmail.com>
2021-04-04 15:04:18 +0300

    feat: implement errors handling in LoginPage

--- src/components/Auth/LoginPage.tsx
+++ src/components/Auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Button,
   Container,
@@ -7,6 +7,7 @@ import {
   makeStyles,
   TextField,
   Typography,
+  Popover,
 } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
@@ -26,21 +27,44 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  typography: {
+    padding: theme.spacing(2),
+  },
 }));
 
 const LoginPage: React.FC<ILoginPage> = ({ history }: ILoginPage) => {
   const classes = useStyles();
   const emailInput = useRef<HTMLInputElement>(null);
   const passwordInput = useRef<HTMLInputElement>(null);
+  const signInButton = (document.getElementById('signInButton') as HTMLButtonElement);
+  const [errors, setErrors] = useState('');
+  const [openPopover, setOpenPopover] = useState(false);
+
+  const id = openPopover ? 'simple-popover' : undefined;
+  const handleClose = () => {
+    setOpenPopover(false);
+  };
 
   const { signIn } = useAuth();
 
   const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    let signInError;
     const emailValue = emailInput.current ? emailInput.current.value : '';
     const passwordValue = passwordInput.current ? passwordInput.current.value : '';
-    const success = await signIn(emailValue, passwordValue);
-    if (success) history.push('/');
+    const success = await signIn(emailValue, passwordValue).catch((error) => {
+      signInError = error.message;
+    });
+    if (success === true && signInError === undefined) {
+      history.push('/');
+      return true;
+    }
+    if (signInError) {
+      setErrors(signInError);
+      setOpenPopover(true);
+      return false;
+    }
+    return false;
   };
   return (
     <Container
@@ -50,6 +74,22 @@ const LoginPage: React.FC<ILoginPage> = ({ history }: ILoginPage) => {
       <Typography component="h1" variant="h5">
         Войти
       </Typography>
+      <Popover
+        id={id}
+        open={openPopover}
+        anchorEl={signInButton}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>{errors}</Typography>
+      </Popover>
       <form
         noValidate
         onSubmit={handleSignIn}
@@ -84,6 +124,7 @@ const LoginPage: React.FC<ILoginPage> = ({ history }: ILoginPage) => {
           fullWidth
           variant="contained"
           color="primary"
+          id="signInButton"
           className={classes.submit}
         >
           Sign In
