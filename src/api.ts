2b6194f6e64d3199457fa5890ce5fa3db55a71e3
Aliaksei Akulau <akulaualiaksei@gmail.com>
2021-04-12 02:03:13 +0300

    feat: immplement localStorage statistic

--- src/api.ts
+++ src/api.ts
@@ -16,17 +16,14 @@ interface IOptionalGame {
   wrong: number
 }
 
-interface IGames {
-  [key: string]: IOptionalGame;
-}
+type IGames = {
+  [key in GameNames]: IOptionalGame;
+};
 
 interface IOptionalWord {
   isDeleted: boolean;
   addTime: Date;
   allTry: number;
-  // for local statistic
-  // allGamesRight: number;
-  // allGamesWrong: number
   games: IGames;
   // games: { [key: string]: IOptionalGame };
 }
@@ -35,6 +32,65 @@ interface IWordBody {
   difficulty: string,
   optional: IOptionalWord
 }
+
+interface ILSStatisticOptionalGames extends IOptionalGame {
+  rightPercent: number;
+  bestSeries: number;
+  newWords: number;
+  wordsList: IWord[];
+}
+interface ILSStatistic {
+  date: Date,
+  allGamesRight: number;
+  allGamesWrong: number;
+  allGamesRightPercent: number;
+  wordsList: IWord[];
+  allNewWords: number;
+  games: {[key in GameNames] : ILSStatisticOptionalGames};
+}
+
+const standartLSStatistic: ILSStatistic = {
+  date: new Date(),
+  allGamesRight: 0,
+  allGamesWrong: 0,
+  allGamesRightPercent: 0,
+  wordsList: [],
+  allNewWords: 0,
+  games: {
+    sprint: {
+      rightPercent: 0,
+      bestSeries: 0,
+      right: 0,
+      wrong: 0,
+      newWords: 0,
+      wordsList: [],
+    },
+    savanna: {
+      rightPercent: 0,
+      bestSeries: 0,
+      right: 0,
+      wrong: 0,
+      newWords: 0,
+      wordsList: [],
+    },
+    oasis: {
+      rightPercent: 0,
+      bestSeries: 0,
+      right: 0,
+      wrong: 0,
+      newWords: 0,
+      wordsList: [],
+    },
+    audioCall: {
+      rightPercent: 0,
+      bestSeries: 0,
+      right: 0,
+      wrong: 0,
+      newWords: 0,
+      wordsList: [],
+    },
+  },
+};
 
 // TODO use games for auth statistic
 
@@ -52,7 +108,7 @@ const standartBody: IWordBody = {
         right: 0,
         wrong: 0,
       },
-      writeWord: {
+      oasis: {
         right: 0,
         wrong: 0,
       },
@@ -62,9 +118,6 @@ const standartBody: IWordBody = {
       },
     },
     allTry: 0,
-    // for local statistic
-    // allGamesRight: 0,
-    // allGamesWrong: 0,
   },
 };
 
@@ -203,4 +256,110 @@ export const setUserWord = async (
     return data;
   }
   return null;
+};
+
+const isToday = (checkDate:Date) => {
+  const today = new Date();
+  return checkDate.getDate() === today.getDate()
+  && checkDate.getMonth() === today.getMonth()
+  && checkDate.getFullYear() === today.getFullYear();
+};
+
+const getLSStatistic = ():ILSStatistic|undefined => {
+  const item = localStorage.getItem('statistic');
+  if (typeof item === 'string') {
+    const result = JSON.parse(item);
+    const dateLS = new Date(result.date);
+    if (isToday(dateLS)) return result;
+    localStorage.removeItem('statistic');
+  }
+  return undefined;
+};
+
+const updateLSStatistic = (item:ILSStatistic) => {
+  localStorage.setItem('statistic', JSON.stringify(item));
+};
+
+const isNewWord = (
+  searchWord: IWord,
+  array: IWord[],
+) => array.find((item) => item.id === searchWord.id);
+
+export const setLSStatistic = (
+  gameName: string,
+  correctAnswers: IWord[],
+  wrongAnswers: IWord[],
+  bestStreak: number,
+) => {
+  const lastLSStat = getLSStatistic();
+  if (lastLSStat) {
+    const {
+      allGamesRight: lastAllGameRigh,
+      allGamesWrong: lastAllGameWrong,
+      allNewWords: lastAllNewWords,
+      wordsList: newWordList,
+    } = lastLSStat;
+    const allGameWords = [...correctAnswers, ...wrongAnswers];
+    const currentGame = GameNames[gameName as keyof typeof GameNames];
+    let newAllNewWords = lastAllNewWords;
+    let newGameAllNewWords = lastLSStat.games[currentGame].newWords;
+    const newGameWordsList = lastLSStat.games[currentGame].wordsList;
+    allGameWords.forEach((item) => {
+      const checkAll = isNewWord(item, newWordList);
+      const checkGame = isNewWord(item, newGameWordsList);
+      if (checkAll === undefined) {
+        newWordList.push(item);
+        newAllNewWords += 1;
+      }
+      if (checkGame === undefined) {
+        newGameWordsList.push(item);
+        newGameAllNewWords += 1;
+      }
+      return item;
+    });
+    const newAllGameRight = lastAllGameRigh + correctAnswers.length;
+    const newAllGamesWrong = lastAllGameWrong + wrongAnswers.length;
+    const newAllGamesRightPercent = ((newAllGameRight
+       / (newAllGameRight + newAllGamesWrong)) * 100).toFixed(2);
+    const newLSStat:ILSStatistic = deepClone(lastLSStat);
+    newLSStat.date = new Date();
+    newLSStat.allGamesRight = newAllGameRight;
+    newLSStat.allGamesWrong = newAllGamesWrong;
+    newLSStat.allGamesRightPercent = +newAllGamesRightPercent || 0;
+    newLSStat.allNewWords = newAllNewWords;
+    newLSStat.wordsList = newWordList;
+
+    newLSStat.games[currentGame].right += correctAnswers.length;
+    newLSStat.games[currentGame].wrong += wrongAnswers.length;
+    const newGameRightPercent = ((newLSStat.games[currentGame].right
+      / (newLSStat.games[currentGame].right + newLSStat.games[currentGame].wrong)) * 100)
+      .toFixed(2);
+    newLSStat.games[currentGame].rightPercent = +newGameRightPercent || 0;
+    if (newLSStat.games[currentGame].bestSeries < bestStreak) {
+      newLSStat.games[currentGame].bestSeries = bestStreak;
+    }
+    newLSStat.games[currentGame].newWords = newGameAllNewWords;
+    newLSStat.games[currentGame].wordsList = newGameWordsList;
+    updateLSStatistic(newLSStat);
+  } else {
+    const newLSStat:ILSStatistic = deepClone(standartLSStatistic);
+    newLSStat.date = new Date();
+    newLSStat.allGamesRight = correctAnswers.length;
+    newLSStat.allGamesWrong = wrongAnswers.length;
+    newLSStat.allNewWords = [...wrongAnswers, ...correctAnswers].length;
+    newLSStat.wordsList = [...wrongAnswers, ...correctAnswers];
+    const rightPercent = ((correctAnswers.length
+      / (correctAnswers.length + wrongAnswers.length)) * 100).toFixed(2);
+    newLSStat.allGamesRightPercent = +rightPercent || 0;
+    const currentGame = GameNames[gameName as keyof typeof GameNames];
+    newLSStat.games[currentGame].right += correctAnswers.length;
+    newLSStat.games[currentGame].wrong += wrongAnswers.length;
+    newLSStat.games[currentGame].wordsList = [...correctAnswers, ...wrongAnswers];
+    newLSStat.games[currentGame].newWords = [...correctAnswers, ...wrongAnswers].length;
+    newLSStat.games[currentGame].rightPercent = +rightPercent || 0;
+    if (newLSStat.games[currentGame].bestSeries < bestStreak) {
+      newLSStat.games[currentGame].bestSeries = bestStreak;
+    }
+    updateLSStatistic(newLSStat);
+  }
 };
