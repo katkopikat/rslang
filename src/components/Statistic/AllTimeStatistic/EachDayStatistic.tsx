b2c80663c2919325d5d2ce2e1e39646882e78812
Katy Rog <katkopikat@gmail.com>
2021-04-12 18:31:51 +0300

    refact: refactoring statistic components

--- src/components/Statistic/AllTimeStatistic/EachDayStatistic.tsx
+++ src/components/Statistic/AllTimeStatistic/EachDayStatistic.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { Chart, ChartPoint } from 'chart.js';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Bar } from 'react-chartjs-2';
 
 interface IEachDayStatistic {
   datesList: Array<string>;
@@ -7,78 +8,82 @@ interface IEachDayStatistic {
 }
 
 const EachDayStatistic = ({ datesList, wordsListByDay }: IEachDayStatistic) => {
-  const createChart = () => {
-    const barContainer = document.querySelector('.charts__container') as HTMLCanvasElement;
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const barChart = new Chart(barContainer, {
-      type: 'bar',
-      data: {
-        labels: datesList,
-        datasets: [
-          {
-            minBarLength: 2,
-            label: 'Изученные слова',
-            data: wordsListByDay as ChartPoint[],
-            backgroundColor: 'rgba(76, 203, 183, 1)',
-            borderColor: ['rgba(76, 203, 183, 1)'],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        legend: {
-          display: false,
-        },
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                fontSize: 10,
-                fontColor: 'rgba(255, 255, 255, 1)',
-                fontFamily: 'Gilroy-Regular',
-              },
-
-              scaleLabel: {
-                display: true,
-                labelString: 'Кол-во слов',
-                fontColor: '#fff',
-                fontFamily: 'Gilroy-Regular',
-                fontSize: 14,
-              },
-            },
-          ],
-          xAxes: [
-            {
-              ticks: {
-                display: false,
-              },
-
-              scaleLabel: {
-                display: true,
-                labelString: 'Период изучения',
-                fontColor: '#fff',
-                fontFamily: 'Gilroy-Regular',
-                fontSize: 14,
-              },
-            },
-          ],
-        },
-      },
-
-    });
-  };
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+  const [fontSize, setFontSize] = useState(14);
+  const [fontSizeY, setFontSizeY] = useState(8);
 
   useEffect(() => {
-    createChart();
-    // TODO
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (isMobile) {
+      setFontSize(8);
+      setFontSizeY(8);
+    } else {
+      setFontSize(14);
+      setFontSizeY(10);
+    }
+  }, [isMobile]);
+
+  const data = {
+    labels: datesList,
+    datasets: [
+      {
+        minBarLength: 2,
+        label: 'Изученные слова',
+        data: wordsListByDay,
+        backgroundColor: 'rgba(76, 203, 183, 1)',
+        borderColor: ['rgba(76, 203, 183, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            fontSize: fontSizeY,
+            fontColor: 'rgba(255, 255, 255, 1)',
+            fontFamily: 'Gilroy-Regular',
+          },
+
+          scaleLabel: {
+            display: true,
+            labelString: 'Кол-во слов',
+            fontColor: '#fff',
+            fontFamily: 'Gilroy-Regular',
+            fontSize,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          ticks: {
+            display: false,
+          },
+
+          scaleLabel: {
+            display: true,
+            labelString: 'Период изучения',
+            fontColor: '#fff',
+            fontFamily: 'Gilroy-Regular',
+            fontSize,
+          },
+        },
+      ],
+    },
+  };
+
+  // useEffect(() => {
+
+  // }, []);
 
   return (
     <>
-      <canvas className="charts__container" />
+      <Bar data={data} options={options} />
     </>
   );
 };
--- src/components/Statistic/AllTimeStatistic/GrowthStatistic.tsx
+++ src/components/Statistic/AllTimeStatistic/GrowthStatistic.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { Chart, ChartPoint } from 'chart.js';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Line } from 'react-chartjs-2';
 
 interface IGrowthStatistic {
   datesList: Array<string>;
@@ -7,80 +8,89 @@ interface IGrowthStatistic {
 }
 
 const GrowthStatistic = ({ datesList, wordsPeriodtList }: IGrowthStatistic) => {
-  const createChart = () => {
-    const chartContainer = document.querySelector('.charts__container') as HTMLCanvasElement;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const lineChart = new Chart(chartContainer, {
-      type: 'line',
-      data: {
-        labels: datesList,
-        datasets: [
-          {
-            minBarLength: 2,
-            label: ' Изучено слов к этому дню ',
-            data: wordsPeriodtList as ChartPoint[],
-            backgroundColor: ['rgba(67, 121, 255, 0.9)'],
-            borderColor: ['rgba(76, 203, 183, 1)'],
-            borderWidth: 3,
-            fill: false,
-            // borderDash: [5, 5],
-            pointBackgroundColor: 'rgba(255, 255, 255, 1)',
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            pointHitRadius: 30,
-            pointBorderWidth: 0,
-            // fontColor: '#FFF',
-          },
-        ],
-      },
-      options: {
-        legend: {
-          display: false,
-        },
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                fontSize: 10,
-                fontColor: 'rgba(255, 255, 255, 1)',
-                fontFamily: 'Gilroy-Regular',
-              },
-              scaleLabel: {
-                display: true,
-                labelString: 'Кол-во слов',
-                fontColor: '#fff',
-                fontFamily: 'Gilroy-Regular',
-                fontSize: 14,
-
-              },
-            },
-          ],
-          xAxes: [
-            {
-              ticks: {
-                display: false,
-              },
-              scaleLabel: {
-                display: true,
-                labelString: 'Период изучения',
-                fontColor: '#fff',
-                fontFamily: 'Gilroy-Regular',
-                fontSize: 14,
-              },
-            },
-          ],
-        },
-      },
-    });
-  };
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+  const [borderWidth, setBorderWidth] = useState(2);
+  const [pointRadius, setPointRadius] = useState(5);
+  const [pointHoverRadius, setPointHoverRadius] = useState(10);
+  const [fontSize, setFontSize] = useState(14);
+  const [fontSizeY, setFontSizeY] = useState(10);
 
   useEffect(() => {
-    createChart();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (isMobile) {
+      setPointRadius(3);
+      setPointHoverRadius(6);
+      setBorderWidth(1);
+      setFontSize(8);
+      setFontSizeY(7);
+    } else {
+      setPointRadius(5);
+      setPointHoverRadius(10);
+      setBorderWidth(3);
+      setFontSize(14);
+      setFontSizeY(10);
+    }
+  }, [isMobile]);
 
-  return <canvas className="charts__container" />;
+  const data = {
+    labels: datesList,
+    datasets: [
+      {
+        data: wordsPeriodtList,
+        label: ' Изучено слов к этому дню ',
+        backgroundColor: ['rgba(67, 121, 255, 0.9)'],
+        borderColor: ['rgba(76, 203, 183, 1)'],
+        fill: false,
+        pointBackgroundColor: 'rgba(255, 255, 255, 1)',
+        borderWidth,
+        pointRadius,
+        pointHoverRadius,
+        minBarLength: 2,
+      },
+    ],
+  };
+
+  const options = {
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            fontSize: fontSizeY,
+            fontColor: 'rgba(255, 255, 255, 1)',
+            fontFamily: 'Gilroy-Regular',
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Кол-во слов',
+            fontColor: '#fff',
+            fontFamily: 'Gilroy-Regular',
+            fontSize,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          ticks: {
+            display: false,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Период изучения',
+            fontColor: '#fff',
+            fontFamily: 'Gilroy-Regular',
+            fontSize,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <Line data={data} options={options} />
+  );
 };
 
 export default GrowthStatistic;
